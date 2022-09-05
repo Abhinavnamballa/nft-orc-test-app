c1286c63585b2e4eaf339f20e608d457d1d45b2e
Abhinav Namballa <namballaabhi@gmail.com>
2022-09-05 16:41:25 -0400

    metamask warning

--- nft-orc-test/src/App.js
+++ nft-orc-test/src/App.js
@@ -16,6 +16,8 @@ import {
 import { ethers } from 'ethers';
 import { ABI } from './ABI';
 import Art from './background/Art';
+import MetaIcon from './images/MetaMask.png'
+import MetaImage from './images/MetamaskDoc.PNG'
 
 
 function App() {
@@ -77,7 +79,7 @@ if (window.ethereum){
   return (
     <div className="App">
 <Art />
-      {typeof window.ethereum !== 'undefined'?
+      {typeof window.ethereum === 'undefined'?
             <Router>
             <Header orcs={orcs} setOrcs={setOrcs} isConnected={isConnected} setConnected={setConnected} accounts={accounts} setAccounts={setAccounts} error={error} setError={setError} loading={loading} setLoading={setLoading} connectWalletHandler={connectWalletHandler} />
             <Routes>
@@ -91,9 +93,33 @@ if (window.ethereum){
             </Router>
 
         :
-        <div>
-          <h1>Please Install MetaMask</h1>
-          <a href='https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en' target={"_blank"}>Click Here To Download MetaMask</a>
+        <div className='orc-body card' style={{width: "90vw", padding: "150px", overflow:"scroll", marginTop: '20px', height:"100vh"}}>
+          <h1 style={{color: "white", margin: '30px'}}>Please Install MetaMask</h1>
+          <h2 style={{color: "white"}}>To use the web3 OrcPortal Test Application you will need to install the Metamask Chrome extension</h2>
+          <div className='mint-btn' >
+          <a href='https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en' target={"_blank"}>Click Here To Download MetaMask <img src={MetaIcon} alt="image" width="40px" style={{position: "absolute", marginLeft:"10px"}}/></a>
+          </div>
+
+          <div>
+            <h4 style={{color: "white"}}>
+            The Link above will direct you to the Chrome store.
+            </h4>
+
+            <img src={MetaImage} alt="image" width="800px" style={{}}/>
+            <h3 style={{color: "white"}}>
+              Why Metamask?
+            </h3>
+            <h5 style={{color: "white"}}>
+            MetaMask is a Chrome Extension with 10+ Million Users.
+            </h5 >
+
+            <h5 style={{color: "white"}}>
+            It is a decentralized Wallet Protocol that will allow you to interact with Web3 Sites and the Blockchain. It is very easy to set up and start using.
+            </h5 >
+
+
+          </div>
+         
         </div>
     
     }
